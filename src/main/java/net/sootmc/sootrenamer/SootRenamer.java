1ee1a290200e56c7b2ef60784936005e767603c5
Jamie <jamie@sootmc.net>
2022-08-15 22:03:01 +0100

    Add command and startup logic Add permissions

--- src/main/java/net/sootmc/sootrenamer/SootRenamer.java
+++ src/main/java/net/sootmc/sootrenamer/SootRenamer.java
@@ -1,5 +1,6 @@
 package net.sootmc.sootrenamer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,26 +11,28 @@ public final class SootRenamer extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
-
-
+        getCommand("rename").setExecutor(this);
+        Bukkit.getLogger().info("[SootRenamer] SootRenamer is now enabled");
+        Bukkit.getLogger().info("[SootRenamer] Made my JamieIsGeek :)");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info("[SootRenamer] SootRenamer is now disabled");
+        Bukkit.getLogger().info("[SootRenamer] Made my JamieIsGeek :)");
     }
 
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-
-        if(sender instanceof Player && sender.hasPermission("sootrenamer.rename")) {
-            Player player = (Player) sender;
-            RenamerTools rt = RenamerTools.getRt();
-            rt.Renamer(player, args);
-        } else {
-            sender.sendMessage("You cannot rename items!");
+        if(command.getLabel().equals("rename")) {
+            if(sender instanceof Player && sender.hasPermission("sootrenamer.rename")) {
+                Player player = (Player) sender;
+                RenamerTools rt = RenamerTools.getRt();
+                rt.Renamer(player, args);
+            } else {
+                sender.sendMessage("You cannot rename items!");
+            }
         }
 
         return true;
