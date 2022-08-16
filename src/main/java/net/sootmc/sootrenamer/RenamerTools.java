94bd05234dd7d9ffcb7fd589ecd511cc81f9980d
Jaap Elst <jaapelst@gmail.com>
2022-08-16 11:34:42 +0200

    Replace nesting with early returns

--- src/main/java/net/sootmc/sootrenamer/RenamerTools.java
+++ src/main/java/net/sootmc/sootrenamer/RenamerTools.java
@@ -10,27 +10,30 @@ public class RenamerTools {
     public static RenamerTools rt;
     private String prefix = ChatColor.WHITE + "[ " + ChatColor.RED + "Soot" + ChatColor.GOLD + "MC" + ChatColor.WHITE + "] ";
 
-
     public void Renamer(Player player, String[] args) {
         String noColour = stringBuilder(args).toString();
+
         if(noColour.contains("#") && !(player.hasPermission("sootrenamer.grad"))) {
             player.sendMessage(prefix + "You do not have permission to use gradients!");
-        } else {
-            ItemStack ci = player.getInventory().getItemInMainHand();
-            if (ci == null || ci.getType().equals(Material.AIR)) {
-                player.sendMessage(prefix + "You cannot rename nothing!");
-            } else {
-                ItemMeta meta = ci.getItemMeta();
-
-                String name = colourize(noColour);
-                meta.setDisplayName(name);
-                ci.setItemMeta(meta);
-
-                player.getInventory().setItemInMainHand(ci);
-                player.setExp(player.getExp() - 1);
-                player.sendMessage(prefix + "Renamed item to '" + name + ChatColor.RESET + "'");
-            }
+            return;
         }
+
+        ItemStack ci = player.getInventory().getItemInMainHand();
+
+        if (ci == null || ci.getType().equals(Material.AIR)) {
+            player.sendMessage(prefix + "You cannot rename nothing!");
+            return;
+        }
+
+        ItemMeta meta = ci.getItemMeta();
+
+        String name = colourize(noColour);
+        meta.setDisplayName(name);
+        ci.setItemMeta(meta);
+
+        player.getInventory().setItemInMainHand(ci);
+        player.setExp(player.getExp() - 1);
+        player.sendMessage(prefix + "Renamed item to '" + name + ChatColor.RESET + "'");
     }
 
 
